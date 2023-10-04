64c90d9aef08ee7a21a8dbc48744159f4b2e9e75
kareemyosri <kareemyosri>
2023-10-04 19:44:51 +0300

    Merge remote-tracking branch 'origin/master'

--- lib/features/home/view/screens/home_screen.dart
+++ lib/features/home/view/screens/home_screen.dart
@@ -13,20 +13,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late HomeCubit cubit;
-  @override
-  void didChangeDependencies() {
-     cubit = HomeCubit.get(context);
-    // TODO: implement didChangeDependencies
-    super.didChangeDependencies();
-  }
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    cubit.close();
-
-    super.dispose();
-  }
+  // late HomeCubit cubit;
+  // @override
+  // void didChangeDependencies() {
+  //    cubit = HomeCubit.get(context);
+  //   // TODO: implement didChangeDependencies
+  //   super.didChangeDependencies();
+  // }
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   cubit.close();
+  //
+  //   super.dispose();
+  // }
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
